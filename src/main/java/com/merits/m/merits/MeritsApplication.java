355ad7d37c468090a57793a98a34218955b6bf32
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-02-17 08:36:25 +0200

    first commit

--- src/main/java/com/merits/m/merits/MeritsApplication.java
+++ src/main/java/com/merits/m/merits/MeritsApplication.java
@@ -2,9 +2,16 @@ package com.merits.m.merits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MeritsApplication {
+public class MeritsApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MeritsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeritsApplication.class, args);
